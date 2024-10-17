d5cc7319939aab6edd9bfcfab0a8f0f1d644e122
Selma2222 <salma.amnay@gmail.com>
2024-10-17 14:02:18 -0400

    page register

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ import Vue from 'nativescript-vue'
 
 import Home from './components/Home'
 import Login from './components/Login'
+import Register from './components/Register'
 
 new Vue({
   render: (h) => h('frame', [h(Login)]),
