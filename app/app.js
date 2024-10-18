1c376a6993b45e97e37c2f74be61894f8402e697
AmariAbedlahmid <amari.abdlahmid@gmail.com>
2024-10-18 02:35:56 +0100

    creer page profil ajouter icone back definir couleur principal bleu

--- app/app.js
+++ app/app.js
@@ -3,7 +3,9 @@ import Vue from 'nativescript-vue'
 import Home from './components/Home'
 import Login from './components/Login'
 import Register from './components/Register'
+import Profile from './components/Profile'
+
 
 new Vue({
-  render: (h) => h('frame', [h(Login)]),
+  render: (h) => h('frame', [h(Profile)]),
 }).$start()
