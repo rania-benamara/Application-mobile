470bc3d800b4178099e35810b4cefd81540c9e5b
Selma2222 <salma.amnay@gmail.com>
2024-10-19 00:18:22 -0400

    barredelogo

---
+++ app/components/LogoBarre.vue
@@ -0,0 +1,26 @@
+<template>
+    <Page actionBarHidden="true">//pas d'action barre
+        <TabView androidTabsPosition="bottom" class="custom-tabview">
+          <TabViewItem title="Acceuil"  iconSource="~/images/acceuil.png">
+            <StackLayout @tap="" orientation="horizontal">
+            </StackLayout>
+          </TabViewItem>
+          <TabViewItem title="Explorer" iconSource="~/images/chercher.png">
+            <StackLayout @tap="" orientation="horizontal">
+            </StackLayout>
+          </TabViewItem>
+          <TabViewItem title="Panier" iconSource="~/images/panier.png">
+            <StackLayout @tap="" orientation="horizontal">
+            </StackLayout>
+          </TabViewItem>
+          <TabViewItem title="Favoris" iconSource="~/images/favoris.png">
+            <StackLayout @tap="" orientation="horizontal">
+            </StackLayout>
+          </TabViewItem>
+          <TabViewItem title="Menu" iconSource="~/images/menu.png">
+            <StackLayout @tap="" orientation="horizontal">
+            </StackLayout>
+          </TabViewItem>
+        </TabView>
+    </Page>
+</template>
